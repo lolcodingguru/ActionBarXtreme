795daa6e7963deb16147d4b45e87ac377c26aebb
lolcodingguru <jakeohud@gmail.com>
2023-07-17 05:59:01 +0100

    - Added an update checker.

--- src/main/java/com/me/actionbarxtreme/commands/announce.java
+++
@@ -1,9 +0,0 @@
-package com.me.actionbarxtreme.commands;
-
-import org.bukkit.entity.Player;
-
-public class announce {
-    public void announce(Player player, String message, int Duration) {
-
-    }
-}
